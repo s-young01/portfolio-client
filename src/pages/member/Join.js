2419e166f3655f5b74fa872dbccd8a9b320e3226
namminimi <119985133+s-young01@users.noreply.github.com>
2023-03-03 17:50:36 +0900

    portfolio-client

--- src/pages/member/Join.js
+++ src/pages/member/Join.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Join.scss';
+import { FcCalendar } from "react-icons/fc";
+import { Link } from 'react-router-dom';
+
 
 const Join = () => {
     return (
@@ -9,19 +12,20 @@ const Join = () => {
                 <table className='join_table'>
                     <tbody>
                         <tr>
-                            <th>이름</th>
+                            <th width='10%'>이름</th>
                             <td>
                                 <input type='text'/>
                             </td>
                         </tr>
                         <tr>
                             <th>닉네임</th>
-                            <td>
+                            <td width='30%'>
                                 <input type='text'/>
+                                <button className='chek_btn'>중복확인</button>
                             </td>
                         </tr>
                         <tr>
-                            <th>이메일</th>
+                            <th>이메일(아이디)</th>
                             <td>
                                 <input type='text'/>
                                 <span>@</span>
@@ -34,29 +38,36 @@ const Join = () => {
                             </td>
                         </tr>
                         <tr>
-                            <th>전화번호</th>
+                            <th>비밀번호</th>
                             <td>
-                                <input type='text'/>
+                                <input type='password'/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>비밀번호 확인</th>
+                            <td>
+                                <input type='password'/>
                             </td>
                         </tr>
                         <tr>
                             <th>생년월일</th>
                             <td>
-                                <input type='text'/>
-                                <spna>년</spna>
-                                <input type='text'/>
-                                <spna>월</spna>
-                                <input type='text'/>
-                                <spna>일</spna>
+                                <input type='text' className='short_input'/>
+                                <span>년</span>
+                                <input type='text' className='short_input'/>
+                                <span>월</span>
+                                <input type='text' className='short_input'/>
+                                <span>일</span>
+                                <FcCalendar className='calendar_icon'/>
                             </td>
                         </tr>
                     </tbody>
                 </table>
-                <div className='join_btn'>
-                    <button>회원가입</button>
-                    <button>취소</button>
-                </div>
             </div>
+            <div className='join_btn'>
+                    <button>회원가입</button>
+                    <button><Link to='/login'>취소</Link></button>
+                </div>
         </div>
     );
 };
